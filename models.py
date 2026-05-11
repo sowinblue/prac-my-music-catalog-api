4384cd38090a62bd5c051912719a67e6da18bcb1
songWooin <sowintildeblue@gmail.com>
2026-05-11 14:23:41 +0900

    feat: models.py Track 테이블 구조 정의

--- models.py
+++ models.py
@@ -1,2 +1,12 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
+
+
+class Track(Base):
+    __tablename__ = "tracks"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    bpm = Column(Integer)
+    key = Column(String)
+    genre = Column(String)
+    duration_sec = Column(Integer)
